99e68d1665224477aeac83038103612d783c0d8a
Comunidade Synapse BR <comunidadesynapsebr@gmail.com>
2026-09-21 10:56:21 -0300

    fix: import GameSummary in root

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'data/cosmic_game.dart';
 import 'screens/home_screen.dart';
 import 'screens/loading_screen.dart';
 import 'screens/result_screen.dart';
