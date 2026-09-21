4f4f9e8dcc0eb2de208ee04833b87a8d1c09531c
Comunidade Synapse BR <comunidadesynapsebr@gmail.com>
2026-09-21 10:52:20 -0300

    feat: add cosmic_game.dart

---
+++ lib/data/cosmic_game.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+enum CosmicType { galaxia, buracoNegro, planeta, quasar }
+
+extension CosmicTypeLabel on CosmicType {
+  String get label {
+    switch (this) {
+      case CosmicType.galaxia: return 'Galáxia';
+      case CosmicType.buracoNegro: return 'Buraco negro';
+      case CosmicType.planeta: return 'Planeta';
+      case CosmicType.quasar: return 'Quasar';
+    }
+  }
+}
+
+class CosmicChallenge {
+  final String imageUrl;
+  final CosmicType answer;
+  const CosmicChallenge(this.imageUrl, this.answer);
+}
+
+const imageBase='https://raw.githubusercontent.com/Comunidadesynapsebr/Astrol-bica-Quiz-do-Cosmos/main/';
+
+const challenges=<CosmicChallenge>[
+  CosmicChallenge(imageBase+'1789997881286.png',CosmicType.galaxia),
+  CosmicChallenge(imageBase+'1789997941367.png',CosmicType.buracoNegro),
+  CosmicChallenge(imageBase+'1789997996142.png',CosmicType.planeta),
+  CosmicChallenge(imageBase+'1789998060730.png',CosmicType.quasar),
+  CosmicChallenge(imageBase+'1789998133047.png',CosmicType.galaxia),
+  CosmicChallenge(imageBase+'1789998161409.png',CosmicType.planeta),
+];
+
+List<CosmicChallenge> shuffledChallenges(){
+  final list=List<CosmicChallenge>.from(challenges);
+  list.shuffle(Random());
+  return list;
+}
+
+class GameSummary{
+  final int score,total,coinsEarned,answered;
+  const GameSummary({required this.score,required this.total,required this.coinsEarned,required this.answered});
+}
